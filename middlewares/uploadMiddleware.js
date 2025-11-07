dfeaa468ffc9050fcef91596b4508c2548bfbd33
pavithra <paviparamasivam138@gmail.com>
2025-11-07 12:15:32 +0530

    Fix multer-storage-cloudinary and Cloudinary config issue

--- middlewares/uploadMiddleware.js
+++ middlewares/uploadMiddleware.js
@@ -2,6 +2,7 @@ import multer from "multer";
 import { CloudinaryStorage } from "multer-storage-cloudinary";
 import cloudinary from "../utils/cloudinaryConfig.js";
 
+
 const storage = new CloudinaryStorage({
   cloudinary,
   params: async (req, file) => ({
